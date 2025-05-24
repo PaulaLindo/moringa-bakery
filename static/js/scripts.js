a917d617fc4b53a269c5b71b8ff94cb7584f3377
P Lindoh Ngcobo <lindokuhlepaula@gmail.com>
2025-05-25 01:33:51 +0200

    [DEV] Lwandle Moringa Website

---
+++ static/js/scripts.js
@@ -0,0 +1,45 @@
+window.addEventListener('scroll', () => {
+    let topBtn = document.getElementById('topBtn');
+    if (window.scrollY > 300) {
+        topBtn.style.opacity = 1;
+        topBtn.style.pointerEvents = 'auto';
+    } else {
+        topBtn.style.opacity = 0;
+        topBtn.style.pointerEvents = 'none';
+    }
+});
+
+document.getElementById('topBtn').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+document.addEventListener("click", (event) => {
+    let navbar = document.querySelector(".navbar-collapse");
+    let toggler = document.querySelector(".navbar-toggler");
+
+    if (!navbar.contains(event.target) && !toggler.contains(event.target)) {
+        if (navbar.classList.contains("show")) {
+        bootstrap.Collapse.getInstance(navbar).hide();
+    }
+}
+});
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 120,
+            behavior: 'smooth'
+        });
+    });
+}
+);
+
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
