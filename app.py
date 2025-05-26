c224e1f121d355dff727414de59f414df8970233
P Lindoh Ngcobo <lindokuhlepaula@gmail.com>
2025-05-26 12:12:26 +0200

    [DEV] Updated the index.html location

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return open('index.html', encoding='utf-8').read()
 
 if __name__ == '__main__':
     app.run(debug=True)
